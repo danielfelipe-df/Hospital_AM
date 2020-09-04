21f170f355bc8f6f9fa4f6721c79708ff431efa1
Daniel Ortiz <dfortizc1@github.com>
2020-09-04 17:13:18 -0500

    Add the Trabajadores header and source files. These define the class of the agents.

---
+++ trabajadores.cpp
@@ -0,0 +1,9 @@
+#include "trabajadores.h"
+
+void trabajadores::init(){
+  //infected = false;
+  //susceptible = true;
+  //exposed = false;
+  //recovered = false;
+  time = 0;
+}
---
+++ trabajadores.h
@@ -0,0 +1,17 @@
+#ifndef TRABAJADORES_H
+#define TRABAJADORES_H
+
+class trabajadores{
+public:
+
+  //Defino qué tipo de persona es
+  //bool infected, susceptible, exposed, recovered;
+
+  //Defino la variable que me dice el tiempo que el agente pasa en el lugar
+  int time;
+
+  //Función de iniciación
+  void init();
+};
+
+#endif
