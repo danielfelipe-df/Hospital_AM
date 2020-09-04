900ffbd87b771559a7dda223efce97e708b06896
Daniel Ortiz <dfortizc@unal.edu.co>
2020-09-04 16:29:20 -0500

    Add the Random64 header and source. The owner is Jose Daniel Muñoz

---
+++ Random64.h
@@ -0,0 +1,17 @@
+#ifndef RANDOM64_H
+#define RANDOM64_H
+
+//Constantes del generador aleatorio
+class Crandom{
+  unsigned long long u,v,w;
+  
+public:
+  Crandom(unsigned long long j);
+  unsigned long long int64();
+  double r() {return 5.42101086242752217E-20 * int64();}
+  unsigned int int32(){return (unsigned int) int64();};
+  double exponencial(float tau);
+  double gauss(float mu,float sigma);
+};
+
+#endif
