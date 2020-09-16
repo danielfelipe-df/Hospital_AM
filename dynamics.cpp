10b2f90e1b2ea2346dbfdde3b7235cca9e165818
Daniel Ortiz <dfortizc@unal.edu.co>
2020-09-16 16:02:13 -0500

    Modify 'reaction' functions for add PT and LT, and generate the new dynamic.

--- dynamics.cpp
+++ dynamics.cpp
@@ -5,7 +5,7 @@
 std::vector<double> contagio(double Sa, double Sb, double Ea, double Eb, double Pa, double Pb, double PTa, double PTb, double PTAa, double PTAb, double La, double Lb, double LTa, double LTb, double LTAa, double LTAb, double LAa, double LAb, double IAa, double IAb, double Na, double Nb, double prev, Crandom &ran, double t){
 
   //Número de proponsidades
-  const int n = 16;
+  const int n = 14;
 
   //Creo el arreglo de las propensidades
   double As[n];
@@ -27,17 +27,17 @@ std::vector<double> contagio(double Sa, double Sb, double Ea, double Eb, double 
   As[7] = USDpg*(1-kappa)*(1-psi)*Pb;
 
   //Propensidad de recuperarse siendo pre-sintomático
-  As[10] = USDplil*kappa*(Pa+PTa+PTAa);
-  As[11] = USDplil*kappa*(Pb+PTb+PTAb);
+  As[8] = USDplil*kappa*(Pa+PTa+PTAa);
+  As[9] = USDplil*kappa*(Pb+PTb+PTAb);
   
   //Propensidad de recuperarse siendo leve
-  As[12] = USDil*(La+LTa+LTAa+LAa);
-  As[13] = USDil*(Lb+LTb+LTAb+LAb);
+  As[10] = USDil*(La+LTa+LTAa+LAa);
+  As[11] = USDil*(Lb+LTb+LTAb+LAb);
 
   //Propensidad de recuperarse siendo infeccioso grave
-  As[14] = USDig*IAa;
-  As[15] = USDig*IAa;
-  
+  As[12] = USDig*IAa;
+  As[13] = USDig*IAa;
+
 
   //Hallo el tiempo en el que va a pasar la siguiente reacción con el método MDM
   double prom = 230.0, sigma = 55.0, A = beta*Sa*eta*prev, B = 0;
@@ -72,8 +72,7 @@ void mother_reaction(grupo &Out, grupo &In, Crandom &ran, trabajadores *family, 
   int agent = Out[index];
   In.push_back(agent);
   Out.erase(Out.begin() + index);
-  family[agent].kind[typeout] = false;
-  family[agent].kind[typein] = true;
+  family[agent].change(typein, typeout);
 }
 
 
@@ -109,4 +108,92 @@ double biseccion(double A, double prom, double sigma, double t, double B, double
 double function(double A, double prom, double sigma, double t, double tau){
   double A2 = std::sqrt(M_PI/2)*A*sigma, USsigma2 = 1.0/(std::sqrt(2)*sigma);
   return A2*(erf((prom-t)*USsigma2) - erf((prom-t-tau)*USsigma2));
+}
+
+
+void reaction0(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Sa, Ea, ran, altos, 0, 1);
+}
+
+
+void reaction1(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Sb, Eb, ran, bajos, 0, 1);
+}
+
+
+void reaction2(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Ea, Pa, ran, altos, 1, 2);
+}
+
+
+void reaction3(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Eb, Pb, ran, bajos, 1, 2);
+}
+
+
+void reaction4(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Pa, La, ran, altos, 2, 5);
+}
+
+
+void reaction5(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Pb, Lb, ran, bajos, 2, 5);
+}
+
+
+void reaction6(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Pa, IAa, ran, altos, 2, 9);
+}
+
+
+void reaction7(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(Pb, IAb, ran, bajos, 2, 9);
+}
+
+
+void reaction8(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  double N = Pa.size() + PTa.size() + PTAa.size();
+  double num = ran.r()*N;
+  if(num < Pa.size()){mother_reaction(Pa, Ra, ran, altos, 2, 10);}
+  else if(num < Pa.size() + PTa.size()){mother_reaction(PTa, Ra, ran, altos, 3, 10);}
+  else{mother_reaction(PTAa, Ra, ran, altos, 4, 10);}
+}
+
+
+void reaction9(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  double N = Pb.size() + PTb.size() + PTAb.size();
+  double num = ran.r()*N;
+  if(num < Pb.size()){mother_reaction(Pb, Rb, ran, bajos, 2, 10);}
+  else if(num < Pb.size() + PTb.size()){mother_reaction(PTb, Rb, ran, bajos, 3, 10);}
+  else{mother_reaction(PTAb, Rb, ran, bajos, 4, 10);}
+}
+
+
+void reaction10(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  double N = La.size() + LTa.size() + LTAa.size() + LAa.size();
+  double num = ran.r()*N;
+  if(num < La.size()){mother_reaction(La, Ra, ran, altos, 5, 10);}
+  else if(num < La.size() + LTa.size()){mother_reaction(LTa, Ra, ran, altos, 6, 10);}
+  else if(num < La.size() + LTa.size() + LTAa.size()){mother_reaction(LTAa, Ra, ran, altos, 7, 10);}
+  else{mother_reaction(LAa, Ra, ran, altos, 8, 10);}
+}
+
+
+void reaction11(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  double N = Lb.size() + LTb.size() + LTAb.size() + LAb.size();
+  double num = ran.r()*N;
+  if(num < Lb.size()){mother_reaction(Lb, Rb, ran, bajos, 5, 10);}
+  else if(num < Lb.size() + LTb.size()){mother_reaction(LTb, Rb, ran, bajos, 6, 10);}
+  else if(num < Lb.size() + LTb.size() + LTAb.size()){mother_reaction(LTAb, Rb, ran, bajos, 7, 10);}
+  else{mother_reaction(LAb, Rb, ran, bajos, 8, 10);}
+}
+
+
+void reaction12(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(IAa, Ra, ran, altos, 9, 10);
+}
+
+
+void reaction13(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
+  mother_reaction(IAb, Rb, ran, bajos, 9, 10);
 }
--- dynamics.h
+++ dynamics.h
@@ -30,136 +30,59 @@ double function(double A, double prom, double sigma, double t, double tau);
 
 
 /* Esta función me hace la reacción de contagio para un susceptible alto */
-inline void reaction0(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Sa, Ea, ran, altos, 0, 1);
-}
+void reaction0(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de contagio para un susceptible bajo */
-inline void reaction1(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Sb, Eb, ran, bajos, 0, 1);
-}
+void reaction1(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de tránsito de un expuesto alto */
-inline void reaction2(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Ea, Pa, ran, altos, 1, 2);
-}
+void reaction2(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de tránsito de un expuesto bajo */
-inline void reaction3(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Eb, Pb, ran, bajos, 1, 2);
-}
+void reaction3(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de tránsito de un pre-sintomático a leve alto */
-inline void reaction4(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Pa, La, ran, altos, 2, 4);
-}
+void reaction4(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de tránsito de un pre-sintomático a leve bajo */
-inline void reaction5(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Pb, Lb, ran, bajos, 2, 4);
-}
+void reaction5(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de tránsito de un pre-sintomático a infeccioso grave aislado alto */
-inline void reaction6(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Pa, IAa, ran, altos, 2, 7);
-}
+void reaction6(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de tránsito de un pre-sintomático a infeccioso grave aislado bajo */
-inline void reaction7(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Pb, IAb, ran, bajos, 2, 7);
-}
+void reaction7(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
-/* Esta función me hace la reacción de tránsito de un leve a aislado leve alto */
-inline void reaction8(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(La, LAa, ran, altos, 4, 6);
-}
+/* Esta función me hace la reacción de tránsito de un pre-sintomático a recuperado alto*/
+void reaction8(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
-/* Esta función me hace la reacción de tránsito de un leve a aislado leve bajo */
-inline void reaction9(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Lb, LAb, ran, bajos, 4, 6);
-}
-
-
-/* Esta función me hace la reacción de pre-sintomático a recuperado alto */
-inline void reaction10(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Pa, Ra, ran, altos, 2, 8);
-}
-
-
-/* Esta función me hace la reacción de pre-sintomático a recuperado bajo */
-inline void reaction11(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Pb, Rb, ran, bajos, 2, 8);
-}
+/* Esta función me hace la reacción de tránsito de un pre-sintomático a recuperado bajo*/
+void reaction9(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de leve a recuperado alto */
-inline void reaction12(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(La, Ra, ran, altos, 4, 8);
-}
+void reaction10(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 /* Esta función me hace la reacción de leve a recuperado bajo */
-inline void reaction13(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(Lb, Rb, ran, bajos, 4, 8);
-}
+void reaction11(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
-/* Esta función me hace la reacción de pre-sintomático testeado y aislado a recuperado alto */
-inline void reaction14(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(PTAa, Ra, ran, altos, 3, 8);
-}
+/* Esta función me hace la reacción de infeccioso grave a recuperado alto */
+void reaction12(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
-/* Esta función me hace la reacción de pre-sintomático testeado y aislado a recuperado bajo */
-inline void reaction15(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(PTAb, Rb, ran, bajos, 3, 8);
-}
-
-
-/* Esta función me hace la reacción de leve aislado a recuperado alto */
-inline void reaction16(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(LAa, Ra, ran, altos, 6, 8);
-}
-
-
-/* Esta función me hace la reacción de leve aislado a recuperado bajo */
-inline void reaction17(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(LAb, Rb, ran, bajos, 6, 8);
-}
-
-
-/* Esta función me hace la reacción de leve aislado y testeado a recuperado alto */
-inline void reaction18(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(LTAa, Ra, ran, altos, 5, 8);
-}
-
-
-/* Esta función me hace la reacción de leve aislado y testeado a recuperado bajo */
-inline void reaction19(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(LTAb, Rb, ran, bajos, 5, 8);
-}
-
-
-/* Esta función me hace la reacción de tránsito de infeccioso grave a recuperado alto */
-inline void reaction20(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(IAa, Ra, ran, altos, 7, 8);
-}
-
-
-/* Esta función me hace la reacción de tránsito de infeccioso grave a recuperado bajo*/
-inline void reaction21(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos){
-  mother_reaction(IAb, Rb, ran, bajos, 7, 8);
-}
-
+/* Esta función me hace la reacción de infeccioso grave a recuperado bajo */
+void reaction13(grupo &Sa, grupo &Sb, grupo &Ea, grupo &Eb, grupo &Pa, grupo &Pb, grupo &PTa, grupo &PTb, grupo &PTAa, grupo &PTAb, grupo &La, grupo &LTa, grupo &LTb, grupo &Lb, grupo &LTAa, grupo &LTAb, grupo &LAa, grupo &LAb, grupo &IAa, grupo &IAb, grupo &Ra, grupo &Rb, Crandom &ran, trabajadores *altos, trabajadores *bajos);
 
 
 #endif
