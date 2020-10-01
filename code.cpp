108e0ee85a31606697ba897d0f19c0cfbd367d0e
Daniel Ortiz <dfortizc@unal.edu.co>
2020-09-30 21:51:35 -0500

    Add 'tj' array for NMGA algorithm and the generalized modifications.

--- code.cpp
+++ code.cpp
@@ -13,7 +13,7 @@ int main(void)
 {
   //Defino las proporciones de tipo bajo y tipo alto
   double pa = 0.18, pb = 0.82;
-  const int Na = pa*N, Nb = pb*N;
+  const unsigned int Na = pa*N, Nb = pb*N;
 
   //Creo los arreglos de cada tipo
   trabajadores altos[Na], bajos[Nb];
@@ -43,16 +43,17 @@ int main(void)
 
   //Defino las variables del acordeón
   double nu = 2, delta = 5;
-  int loops = T/(nu+delta);
+  unsigned int loops = T/(nu+delta);
 
   //Defino las variables para el testeo masivo
-  int tests = 500;
+  unsigned int tests = 500;
   double dt = nu/((double)tests);
-  //double timetest[tests];
-  //for(int i=0; i<tests; i++){timetest[i] = ((double)i)*dt;}
+
+  //Defino la variable de tiempo propio de cada reacción(tj)
+  double tj[14];
 
   //Defino el número de corridas
-  int ensemble = 1;
+  unsigned int ensemble = 1000;
 
   //Creo el arreglo de las funciones de reacción
   reactions react[14] = {reaction0, reaction1, reaction2, reaction3, reaction4, reaction5, reaction6, reaction7, reaction8, reaction9,
@@ -60,7 +61,7 @@ int main(void)
 
   //Variables auxiliares
   std::vector<double> ti_in;
-  int n1, n2, index;
+  unsigned int n1, n2, index;
   double aux;
   
   std::ofstream fout;
@@ -68,6 +69,7 @@ int main(void)
 
   //Genero las corridas
   for(unsigned int i=0; i<ensemble; i++){
+    std::cout << i << std::endl;
 
     //Le asigno a cada entrada el índice como valor
     susal.resize(Na);    susba.resize(Nb);
@@ -75,7 +77,7 @@ int main(void)
     for(unsigned int j=0; j<Nb; j++){susba[j] = j;    bajos[j].init();}
 
     //name = "Data/datos_" + std::to_string(i) + ".csv";
-    name = "prueba4.csv";
+    name = "prueba.csv";
     fout.open(name);
 
     //Inicio el tiempo
@@ -91,6 +93,9 @@ int main(void)
     fout << levAal.size() << '\t' << levAba.size() << '\t';
     fout << infAal.size() << '\t' << infAba.size() << '\t';
     fout << recal.size() << '\t' << recba.size() << std::endl;
+    
+    //Inicio los tiempos propios de cada reacción
+    for(unsigned int j=0; j<14; j++){tj[j] = 0.0;}   
 
     for(unsigned int j=0; j<loops; j++){
       
@@ -99,10 +104,10 @@ int main(void)
       n1 = 0;
       while(aux < nu){
 	//Obtengo el tiempo e índice de la reacción
-	ti_in = contagio(susal.size(), susba.size(), expal.size(), expba.size(), preal.size(), preba.size(), preTal.size(), preTba.size(), preTAal.size(), preTAba.size(), leval.size(), levba.size(), levTal.size(), levTba.size(), levTAal.size(), levTAba.size(), levAal.size(), levAba.size(), infAal.size(), infAba.size(), Na, Nb, prev, gseed, t);
+	ti_in = contagio(susal.size(), susba.size(), expal.size(), expba.size(), preal.size(), preba.size(), preTal.size(), preTba.size(), preTAal.size(), preTAba.size(), leval.size(), levba.size(), levTal.size(), levTba.size(), levTAal.size(), levTAba.size(), levAal.size(), levAba.size(), infAal.size(), infAba.size(), Na, Nb, prev, gseed, t, tj);
       
 	//Si se tiene el tiempo máximo como tiempo mínimo, entonces termino la simulación
-	if(ti_in[0] == 1e6){break;}
+	if(ti_in[0] == 1e6){break;}	
 	
 	//Actualizo los tiempos de los testeados, y si ya les dieron resultado los aislo
 	tested_isolated(preTal, preTAal, altos, ti_in[0], 3, 4);
@@ -115,7 +120,7 @@ int main(void)
 
 	//Sumo el tiempo de la reacción
 	t += ti_in[0];
-	aux += ti_in[0];
+	aux += ti_in[0];	
 
 	//Genero lo tests continuos para los leves
 	if((int)ti_in[1] == 4){continue_reaction(leval, levTal, altos, gseed);}
@@ -128,7 +133,7 @@ int main(void)
 	  else{massive_reaction(susba, expba, preba, preTba, levba, levTba, recba, gseed, bajos);}
 	}
 	n1 = n2;
-      
+	
 	fout << t << '\t' << susal.size() << '\t' << susba.size() << '\t';
 	fout << expal.size() << '\t' << expba.size() << '\t';
 	fout << preal.size() << '\t' << preba.size() << '\t';
@@ -145,14 +150,17 @@ int main(void)
 	ti_in.clear();
       }
 
+      //Si el vector de tiempo e índice no se borró, es porque se rompió el ciclo
+      if(ti_in.size() != 0){break;}
+
       //Región sin testeo masivo
       aux = 0.0;
       while(aux < delta){
 	//Obtengo el tiempo e índice de la reacción
-	ti_in = contagio(susal.size(), susba.size(), expal.size(), expba.size(), preal.size(), preba.size(), preTal.size(), preTba.size(), preTAal.size(), preTAba.size(), leval.size(), levba.size(), levTal.size(), levTba.size(), levTAal.size(), levTAba.size(), levAal.size(), levAba.size(), infAal.size(), infAba.size(), Na, Nb, prev, gseed, t);
+	ti_in = contagio(susal.size(), susba.size(), expal.size(), expba.size(), preal.size(), preba.size(), preTal.size(), preTba.size(), preTAal.size(), preTAba.size(), leval.size(), levba.size(), levTal.size(), levTba.size(), levTAal.size(), levTAba.size(), levAal.size(), levAba.size(), infAal.size(), infAba.size(), Na, Nb, prev, gseed, t, tj);
       
 	//Si se tiene el tiempo máximo como tiempo mínimo, entonces termino la simulación
-	if(ti_in[0] == 1e6){break;}
+	if(ti_in[0] == 1e6){break;}	
 
 	//Actualizo los tiempos de los testeados, y si ya les dieron resultado los aislo
 	tested_isolated(preTal, preTAal, altos, ti_in[0], 3, 4);
@@ -161,7 +169,7 @@ int main(void)
 	tested_isolated(levTba, levTAba, bajos, ti_in[0], 5, 6);
       
 	//Genero la reacción según el índice que acabo de obtener
-	react[(int)ti_in[1]](susal, susba, expal, expba, preal, preba, preTal, preTba, preTAal, preTAba, leval, levba, levTal, levTba, levTAal, levTAba, levAal, levAba, infAal, infAba, recal, recba, gseed, altos, bajos);
+	react[(int)ti_in[1]](susal, susba, expal, expba, preal, preba, preTal, preTba, preTAal, preTAba, leval, levba, levTal, levTba, levTAal, levTAba, levAal, levAba, infAal, infAba, recal, recba, gseed, altos, bajos);	
 
 	//Genero lo tests continuos para los leves
 	if((int)ti_in[1] == 4){continue_reaction(leval, levTal, altos, gseed);}
@@ -170,7 +178,7 @@ int main(void)
 	//Sumo el tiempo de la reacción
 	t += ti_in[0];
 	aux += ti_in[0];
-      
+	
 	fout << t << '\t' << susal.size() << '\t' << susba.size() << '\t';
 	fout << expal.size() << '\t' << expba.size() << '\t';
 	fout << preal.size() << '\t' << preba.size() << '\t';
@@ -186,6 +194,9 @@ int main(void)
 	//Borro el vector de tiempo e índice
 	ti_in.clear();
       }
+      
+      //Si el vector de tiempo e índice no se borró, es porque se rompió el ciclo
+      if(ti_in.size() != 0){break;}
     }
     fout.close();
 
@@ -202,7 +213,6 @@ int main(void)
     infAal.clear();    infAba.clear();
     recal.clear();    recba.clear();
   }
-
 
   return 0;
 }
