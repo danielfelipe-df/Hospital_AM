ac18358e68825da5912a1df5a6d13d08bd414bb2
Daniel Ortiz <dfortizc@unal.edu.co>
2020-11-10 15:52:25 -0500

    Modify the aux_main function for trace agents taken into account an small network.

--- trace.cpp
+++ trace.cpp
@@ -23,68 +23,57 @@ void main_trace(std::vector<grupo> &Val, std::vector<grupo> &Vba, trabajadores *
 
 
 void aux_main(int num, std::vector<grupo> &Val, std::vector<grupo> &Vba, trabajadores *altos, trabajadores *bajos, Crandom &ran, double cons1, double cons2, bool type, unsigned int index){
-  unsigned int contador, aux, ind, my_size, Gsize;
-  double value1, value2, value3;
+  unsigned int contador, aux, aux2, ind, my_size, Gsize, j;
+  double value;
+  grupo vaux;
   if(type){Gsize = Val[index].size();}
   else{Gsize = Vba[index].size();}
 
+  unsigned int trace_net = 14;
+
   for(unsigned int i=Gsize-num; i<Gsize; i++){
-    contador = 0;
     if(type){my_size = altos[Val[index][i]].my_inf.size();}
     else{my_size = bajos[Vba[index][i]].my_inf.size();}
 
-    for(unsigned int j=0; j<my_size && contador<trace; j++){
+    for(j=0; j<my_size && j<trace_net; j++){
       if(type){ind = altos[Val[index][i]].my_inf[j];}
       else{ind = bajos[Vba[index][i]].my_inf[j];}
+      vaux.push_back(ind);
+    }
 
+    while(j<trace_net){
+      value = ran.r()*(cons1*Na + cons2*Nb);
+      if(value < cons1*Na){ind = (int)(ran.r()*Na);}
+      else{ind = (int)(ran.r()*Nb) + Na;}
+      vaux.push_back(ind);
+      j++;
+    }
+
+    contador = 0;
+    while(vaux.size() > 0 && contador < trace){
+      aux2 = (int)(ran.r()*vaux.size());
+      ind = vaux[aux2];
       if(ind < Na){aux = reaction_trace(Val, altos, ran, ind);}
       else{aux = reaction_trace(Vba, bajos, ran, ind-Na);}
       contador += aux;
+      vaux.erase(vaux.begin() + aux2);
     }
-
-    while(contador<trace){
-      value1 = Val[4].size() + Val[7].size() + Val[10].size() + Val[11].size() + Val[14].size(); //Todos los aislados altos
-      value2 = Vba[4].size() + Vba[7].size() + Vba[10].size() + Vba[11].size() + Vba[14].size(); //Todos los aislados bajos
-      value3 = ran.r()*(cons1*(Na-value1) + cons2*(Nb-value2));
-      if(value3 < cons1*(Na-value1)){aux = reaction_trace(Val, altos, ran, -1);}
-      else{aux = reaction_trace(Vba, bajos, ran, -1);}
-      contador += aux;
-    }
+    vaux.clear();
   }
 }
 
 
 int reaction_trace(std::vector<grupo> &V, trabajadores *family, Crandom &ran, int index){
-  int agent;
-  if(index == -1){
-    grupo aux;
-    int ind;
-    for(unsigned int i=0; i<=3; i++){std::copy(V[i].begin(), V[i].end(), std::back_inserter(aux));} //Susceptibles y expuestos
-    std::copy(V[5].begin(), V[5].end(), std::back_inserter(aux)); //Presintomáticos
-    std::copy(V[6].begin(), V[6].end(), std::back_inserter(aux)); //Presintomáticos testeados
-    std::copy(V[8].begin(), V[8].end(), std::back_inserter(aux)); //Leves
-    std::copy(V[9].begin(), V[9].end(), std::back_inserter(aux)); //Leves testeados
-    std::copy(V[12].begin(), V[12].end(), std::back_inserter(aux)); //Recuperados no-detectados
-    std::copy(V[13].begin(), V[13].end(), std::back_inserter(aux)); //Recuperados testeados
-
-    ind = (int)(ran.r()*aux.size());
-    agent = aux[ind];
-    aux.clear();
-  }
-  else{
-    agent = index;
-  }
-
-  if(family[agent].kind == 0){aux_trace(V[0], V[1], family, 0, 1, agent, true);    return 1;} //Susceptible
-  else if(family[agent].kind == 1){aux_trace(V[0], V[1], family, 0, 1, agent, false);    return 1;} //Susceptible testeado
-  else if(family[agent].kind == 2){aux_trace(V[2], V[4], family, 2, 4, agent, true);    return 1;} //Expuesto
-  else if(family[agent].kind == 3){aux_trace(V[3], V[4], family, 3, 4, agent, true);    return 1;} //Expuesto testeado
-  else if(family[agent].kind == 5){aux_trace(V[5], V[7], family, 5, 7, agent, true);    return 1;} //Presintomático
-  else if(family[agent].kind == 6){aux_trace(V[6], V[7], family, 6, 7, agent, true);    return 1;} //Presintomático testeado
-  else if(family[agent].kind == 8){aux_trace(V[8], V[10], family, 8, 10, agent, true);    return 1;} //Leve
-  else if(family[agent].kind == 9){aux_trace(V[9], V[10], family, 9, 10, agent, true);    return 1;} //Leve testeado
-  else if(family[agent].kind == 12){aux_trace(V[12], V[13], family, 12, 13, agent, true);    return 1;} //Recuperado no-detectado
-  else if(family[agent].kind == 13){aux_trace(V[12], V[13], family, 12, 13, agent, false);    return 1;} //Recuperado testeado
+  if(family[index].kind == 0){aux_trace(V[0], V[1], family, 0, 1, index, true);    return 1;} //Susceptible
+  else if(family[index].kind == 1){aux_trace(V[0], V[1], family, 0, 1, index, false);    return 1;} //Susceptible testeado
+  else if(family[index].kind == 2){aux_trace(V[2], V[4], family, 2, 4, index, true);    return 1;} //Expuesto
+  else if(family[index].kind == 3){aux_trace(V[3], V[4], family, 3, 4, index, true);    return 1;} //Expuesto testeado
+  else if(family[index].kind == 5){aux_trace(V[5], V[7], family, 5, 7, index, true);    return 1;} //Presintomático
+  else if(family[index].kind == 6){aux_trace(V[6], V[7], family, 6, 7, index, true);    return 1;} //Presintomático testeado
+  else if(family[index].kind == 8){aux_trace(V[8], V[10], family, 8, 10, index, true);    return 1;} //Leve
+  else if(family[index].kind == 9){aux_trace(V[9], V[10], family, 9, 10, index, true);    return 1;} //Leve testeado
+  else if(family[index].kind == 12){aux_trace(V[12], V[13], family, 12, 13, index, true);    return 1;} //Recuperado no-detectado
+  else if(family[index].kind == 13){aux_trace(V[12], V[13], family, 12, 13, index, false);    return 1;} //Recuperado testeado
   else{return 0;}
 }
 
