b804c2492959d72935b506c7d682c401ea0ad26d
Daniel Ortiz <dfortizc@unal.edu.co>
2020-12-01 16:46:10 -0500

    Change the ais value (1.0 - 1e-4 -> 0.9), N95 (1.0 - 0.143*3 -> 0.051) and TBQ (1.0 - 0.143 -> 0.39). Create the variables HW and SDP. Change AG (Tt: 0.5 and Xi: 0.65) to PCR (Tt: 1.82 and Xi: 0.9).

--- inc/bases.h
+++ inc/bases.h
@@ -27,7 +27,6 @@ const int Nb = N*0.82; //Número de personas de riesgo bajo
 
 /* Tasas de la dinámica ***************************************************/
 
-const double beta = 0.88*(1.0 - 0.102); //Beta de infección
 const double De = 2.9; //Periodo latente
 const double Dpl = 1.3; //Periodo infeccioso pre-sintomático para leves
 const double Dpg = 2.3; //Periodo infecciosos pre-sintomático para graves
@@ -44,17 +43,20 @@ const double USDplil = 1.0/(Dpl+Dil); //Inverso del periodo infeccioso total
 
 const double kappa = 0.2; //Proporción de contagiados que son asintomáticos
 const double psi = 0.95; //Proporción de sintomáticos que son leves
-const double xi = 0.65; //Sensibilidad de la prueba
+const double xi = 0.9; //Sensibilidad de la prueba
 const double theta = 0.5; //Cobertura
 const double iota = 0.6; //Proporción de sintomáticos leves testeados continuamente
-const double Tt = 0.5; //Tiempo de reporte de la prueba
+const double Tt = 1.82; //Tiempo de reporte de la prueba
 const double TM = 0.5; //Tiempo mínimo que deben pasar los agentes en cada estado
-const double N95 = 1.0 - 0.96; //Probabilidad de contagiarse usando tapabocas N95
-const double TBQ = 1.0 - 0.67; //Probabilidad de contagiarse usando tapabocas quirúrgico
+const double N95 = 0.051; //Reducción en la probabilidad de contagiarse usando tapabocas N95
+const double TBQ = 0.39; //Reducción en el probabilidad de contagiarse usando tapabocas quirúrgico
+const double HW = 1.0 - 0.24; //Reducción en la probabilidad de contagiarse si se lava las manos
+const double SDP = 1.0 - 0.102; //Reducción en la probabilidad de contagiarse si mantiene distanciamiento
+const double beta = 1.3*HW*SDP; //Beta de infección
 
 /* Tasas de contacto ******************************************************/
 
-const double alpha = 1.0 - 1e-4; //Adherencia al aislamiento
+const double alpha = 0.9; //Adherencia al aislamiento
 const double mu = 0.1; //Tasa de contacto cruzada
 const double chi = 0.7; //Tasa de contacto de tipo bajo
 const double eta = 0.7; //Tasa de contacto con externos
